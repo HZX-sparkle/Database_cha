818528906b0b2a7ea62dc8336f7bc41df75ffd51
HZX-sparkle <2841287903@qq.com>
2024-07-05 11:48:27 -0400

    proj3 倒排索引测试

--- proj3/bitmap/tests/BitmapIndex_test.cc
+++ proj3/bitmap/tests/BitmapIndex_test.cc
@@ -16,6 +16,7 @@ TEST(hello, bitmap)
     printf("hello, Bitmap\n");
 }
 
+// 读取文件测试
 TEST(BitmapIndexTest, readCSV1)
 {
     BitmapIndex index;
@@ -52,9 +53,29 @@ TEST(BitmapIndexTest, readCSV1)
     EXPECT_EQ(tags[3], "tag6");
 }
 
+// 读取不存在的文件
 TEST(BitmapIndexTest, readCSV2)
 {
     BitmapIndex index2;
     bool ret = index2.readCSV("unknown file");
     EXPECT_FALSE(ret);
+}
+
+// 倒排索引测试
+TEST(BitmapIndexTest, buildInvertedIndex)
+{
+    BitmapIndex index;
+    index.readCSV("../data/tags.csv");
+    index.buildInvertedIndex();
+    auto bitmap = index.getBitmap("tag1");
+    bitmap.printf();
+    printf("\n");
+    EXPECT_TRUE(bitmap.contains(1));
+    EXPECT_TRUE(bitmap.contains(2));
+
+    bitmap = index.getBitmap("tag2");
+    bitmap.printf();
+    printf("\n");
+    EXPECT_TRUE(bitmap.contains(1));
+    EXPECT_TRUE(bitmap.contains(3));
 }
