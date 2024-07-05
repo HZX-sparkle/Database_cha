a4f3953c80c2ccd225a76fac9cde8445dada4cdc
HZX-sparkle <2841287903@qq.com>
2024-07-05 10:13:32 -0400

    proj3 demo

---
+++ proj3/bitmap/tests/BitmapIndex_test.cc
@@ -0,0 +1,17 @@
+////
+// @file BitmapIndex_test.cc
+// @brief
+// BitmapIndex 单元测试
+//
+// @author niexw
+// @email niexiaowen@uestc.edu.cn
+//
+#include <gtest/gtest.h>
+#include <BitmapIndex.h>
+// #include <test_target.h>
+
+TEST(hello, bitmap)
+{
+    ASSERT_TRUE(true);
+    printf("hello, Bitmap\n");
+}
